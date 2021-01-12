7c136cc2464ed6c78eaeeeac8e7b1e5b39eb7dcc
jxhnkndl <jkroyston@gmail.com>
2021-01-11 20:22:08 -0500

    Add function to render recipe details from API response in user interface

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,5 @@
 $(document).ready(function() {
 
-$(".button-left").ready(function() {
- $("#food-side").show();
- $("#drinks-side").hide();
- $(".button-left").css({"background-color": "black", "color": "white"});
- $(".button-right").css({"background-color": "gray", "color": "white"});
- });
-$('.button-left').click(function() {
- $('#food-side').show(500);
- $('#drinks-side').hide(500);
- $(".button-left").css({"background-color": "black", "color": "white"});
- $(".button-right").css({"background-color": "gray", "color": "white"});
- });
-$('.button-right').click(function() {
- $('#drinks-side').show(500);
- $('#food-side').hide(500);
- $(".button-left").css({"background-color": "gray", "color": "white"});
- $(".button-right").css({"background-color": "black", "color": "white"});
-});
   var apiKey =  "9973533";
 
   // Make API call to get recipe data
@@ -34,8 +16,10 @@ $('.button-right').click(function() {
 
       var id;
 
+      var index = Math.floor(Math.random() * response.meals.length);
+
       if (mode === "recipes") {
-        id = response.meals[0].idMeal;
+        id = response.meals[index].idMeal;
       } else if (mode === "drinks") {
         id = response.drinks[0].idDrink;
       }
@@ -55,20 +39,101 @@ $('.button-right').click(function() {
     });
   }
 
+
   function displayRecipe(recipe) {
-    $("#title").text(recipe.meals[0].strMeal)
-    $("#thumbnail").attr("src", recipe.meals[0].strMealThumb)
-    $("#instructions").text(recipe.meals[0].strInstructions)
-    
-    if (recipe.meals[0].strYoutube) {
-        var li = $("<li>").addClass("list-group-item pl-0");
-        var a = $("<a>").addClass("text-success bold");
-        a.text("New Video");
-        a.attr("href", recipe.meals[0].strYoutube)
-        li.append(a);
-        $("#recipe-info").prepend(li);
-        
+    var meal = recipe.meals[0];
+
+    var ingredients = [
+      meal.strIngredient1,
+      meal.strIngredient2,
+      meal.strIngredient3,
+      meal.strIngredient4,
+      meal.strIngredient5,
+      meal.strIngredient6,
+      meal.strIngredient7,
+      meal.strIngredient8,
+      meal.strIngredient9,
+      meal.strIngredient10,
+      meal.strIngredient11,
+      meal.strIngredient12,
+      meal.strIngredient13,
+      meal.strIngredient14,
+      meal.strIngredient15,
+      meal.strIngredient16,
+      meal.strIngredient17,
+      meal.strIngredient18,
+      meal.strIngredient19,
+      meal.strIngredient20,
+    ];
+
+    var measurements = [
+      meal.strMeasure1,
+      meal.strMeasure2,
+      meal.strMeasure3,
+      meal.strMeasure4,
+      meal.strMeasure5,
+      meal.strMeasure6,
+      meal.strMeasure7,
+      meal.strMeasure8,
+      meal.strMeasure9,
+      meal.strMeasure10,
+      meal.strMeasure11,
+      meal.strMeasure12,
+      meal.strMeasure13,
+      meal.strMeasure14,
+      meal.strMeasure15,
+      meal.strMeasure16,
+      meal.strMeasure17,
+      meal.strMeasure18,
+      meal.strMeasure19,
+      meal.strMeasure20,
+    ];
+
+    $("#title").text(meal.strMeal);
+    $("#thumbnail").attr("src", meal.strMealThumb);
+    $("#instructions").text(meal.strInstructions);
+
+    var listTitle = $("<li>");
+    listTitle.addClass("list-group-item text-success bold pl-0");
+    listTitle.text("Ingredients:")
+    $("#recipe-info").append(listTitle);
+
+    $.each(ingredients, function(i, ingredient) {
+      if (ingredient) {
+        var li = $("<li>");
+        li.addClass("list-group-item pt-0 pl-0");
+        li.text(measurements[i] + " " + ingredient);
+        $("#recipe-info").append(li);
+      }
+    });
+
+    if (meal.strSource) {
+      var li = $("<li>").addClass("list-group-item pl-0");
+      var a = $("<a>").addClass("text-success bold");
+      a.text("View Recipe Source");
+      a.attr("href", meal.strSource)
+      li.append(a);
+      $("#recipe-info").append(li);
     }
+
+    if (meal.strYoutube) {
+      var li = $("<li>").addClass("list-group-item pl-0");
+      var a = $("<a>").addClass("text-success bold");
+      a.text("View Video");
+      a.attr("href", meal.strYoutube)
+      li.append(a);
+      $("#recipe-info").append(li);
+    }
+
+    var instructions = $("<li>").addClass("list-group-item pl-0");
+    var instructionsLink = $("<a>").addClass("text-success bold");
+    instructionsLink.attr("data-toggle", "collapse");
+    instructionsLink.attr("data-target", "#drawer");
+    instructionsLink.attr("aria-expanded", "false");
+    instructionsLink.attr("aria-controls", "drawer");
+    instructionsLink.text("View Recipe Instructions");
+    instructions.append(instructionsLink);
+    $("#recipe-info").append(instructions);
   }
 
 
@@ -97,5 +162,28 @@ $('.button-right').click(function() {
     } else if (!ingredient && !category) {
       console.log("Invalid input.");
     }
+  });
+
+
+  // Event Listeners: Food/Drink Buttons
+  $(".button-left").ready(function() {
+    $("#food-side").show();
+    $("#drinks-side").hide();
+    $(".button-left").css({"background-color": "black", "color": "white"});
+    $(".button-right").css({"background-color": "gray", "color": "white"});
+  });
+  
+  $('.button-left').click(function() {
+    $('#food-side').show(500);
+    $('#drinks-side').hide(500);
+    $(".button-left").css({"background-color": "black", "color": "white"});
+    $(".button-right").css({"background-color": "gray", "color": "white"});
+  });
+
+  $('.button-right').click(function() {
+    $('#drinks-side').show(500);
+    $('#food-side').hide(500);
+    $(".button-left").css({"background-color": "gray", "color": "white"});
+    $(".button-right").css({"background-color": "black", "color": "white"});
   });
 });
